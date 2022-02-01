fab078985eeb6f70cc69591829fe0bf01bbd4312
DevExpressExampleBot <fakeEmail>
2022-02-01 18:09:37 +0300

    Source auto update [skip ci]

--- VB/PdfProcessorPrinterOptions/Program.vb
+++ VB/PdfProcessorPrinterOptions/Program.vb
@@ -1,28 +1,24 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to use the PDF printer settings
-
 Imports DevExpress.Pdf
 
 Namespace PdfProcessorPrinterOptions
+
     Friend Class Program
 
-        Shared Sub Main(ByVal args() As String)
+        Shared Sub Main(ByVal args As String())
             ' Create a Pdf Document Processor instance and load a PDF into it.
-            Dim documentProcessor As New PdfDocumentProcessor()
+            Dim documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
             documentProcessor.LoadDocument("..\..\Demo.pdf")
-
             ' Declare the PDF printer settings.
-            Dim pdfPrinterSettings As New PdfPrinterSettings()
-
+            Dim pdfPrinterSettings As PdfPrinterSettings = New PdfPrinterSettings()
             ' Specify the PDF printer settings.
             pdfPrinterSettings.PageOrientation = PdfPrintPageOrientation.Portrait
-            pdfPrinterSettings.PageNumbers = New Integer() { 1, 3, 4, 5 }
-
+            pdfPrinterSettings.PageNumbers = New Integer() {1, 3, 4, 5}
             ' Setting the PdfPrintScaleMode property to CustomScale requires 
             ' specifying the Scale property, as well.
             pdfPrinterSettings.ScaleMode = PdfPrintScaleMode.CustomScale
             pdfPrinterSettings.Scale = 90
-
             ' Print the document using the specified printer settings.
             documentProcessor.Print(pdfPrinterSettings)
         End Sub
--- VB/PdfProcessorPrinterOptions/Properties/AssemblyInfo.vb
+++ VB/PdfProcessorPrinterOptions/Properties/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to use the PDF printer settings
 ' 
 ' This example illustrates the application programming interface (API) available
@@ -9,31 +9,26 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=T172274
-
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("PdfProcessorPrinterOptions")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("PdfProcessorPrinterOptions")>
-<Assembly: AssemblyCopyright("Copyright ©  2014")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("PdfProcessorPrinterOptions")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("PdfProcessorPrinterOptions")>
+<Assembly:AssemblyCopyright("Copyright ©  2014")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("5d82decb-0a15-4c22-bb6e-873984054c17")>
-
+<Assembly:Guid("5d82decb-0a15-4c22-bb6e-873984054c17")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -44,5 +39,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
