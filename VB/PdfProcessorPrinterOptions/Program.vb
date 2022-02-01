2a2ec5d09a0254fc7e9ffd5e52853a4396f9a5fd
DevExpressExampleBot <fakeEmail>
2022-02-01 21:35:55 +0300

    Source auto update [skip ci]

--- VB/PdfProcessorPrinterOptions/Program.vb
+++ VB/PdfProcessorPrinterOptions/Program.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to use the PDF printer settings
 ' 
 ' This example illustrates the application programming interface (API) available
@@ -9,39 +9,35 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=T172274
-
-#Region "#Reference"
+'#Region "#Reference"
 Imports DevExpress.Pdf
+
 ' ...
-#End Region ' #Reference
-
+'#End Region  ' #Reference
 Namespace PdfProcessorPrinterOptions
+
     Friend Class Program
-        #Region "#Code"
-        Shared Sub Main(ByVal args() As String)
+
+'#Region "#Code"
+        Shared Sub Main(ByVal args As String())
             ' Create a Pdf Document Processor instance and load a PDF into it.
-            Dim documentProcessor As New PdfDocumentProcessor()
+            Dim documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
             documentProcessor.LoadDocument("..\..\Demo.pdf")
-
             ' Declare the PDF printer settings.
-            Dim pdfPrinterSettings As New PdfPrinterSettings()
-
+            Dim pdfPrinterSettings As PdfPrinterSettings = New PdfPrinterSettings()
             ' Specify the PDF printer settings.
             pdfPrinterSettings.PageOrientation = PdfPrintPageOrientation.Portrait
-            pdfPrinterSettings.PageNumbers = New Integer() { 1, 3, 4, 5 }
-
+            pdfPrinterSettings.PageNumbers = New Integer() {1, 3, 4, 5}
             ' Changing (reducing) the default printing DPI is only required to 
             ' maintain a specific print quality or speed-up document rendering.
             pdfPrinterSettings.PrintingDpi = 600
-
             ' Setting the PdfPrintScaleMode property to CustomScale requires 
             ' specifying the Scale property, as well.
             pdfPrinterSettings.ScaleMode = PdfPrintScaleMode.CustomScale
             pdfPrinterSettings.Scale = 90
-
             ' Print the document using the specified printer settings.
             documentProcessor.Print(pdfPrinterSettings)
         End Sub
-        #End Region ' #Code
+'#End Region  ' #Code
     End Class
 End Namespace
